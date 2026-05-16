f01ebd97b45f62393b38c6d6bf25c5c05fdf84a4
Bashar Ahmed <Basharahmed9908@gmail.com>
2026-05-16 17:53:02 +0300

    add language switcher

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { LangProvider } from "./Components/LangContext";ل
+import { LangProvider } from "./Components/LangContext";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="678553599072-5ncop5mo1558m4sdvrim2iidscj1224t.apps.googleusercontent.com">
