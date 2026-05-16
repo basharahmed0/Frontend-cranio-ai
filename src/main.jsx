0b94f23f74dc258027424205964148a347f6b675
Bashar Ahmed <Basharahmed9908@gmail.com>
2026-05-16 20:28:42 +0300

    fix bug

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { LangProvider } from "./Components/LangContext";
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <GoogleOAuthProvider clientId="678553599072-5ncop5mo1558m4sdvrim2iidscj1224t.apps.googleusercontent.com">
-      <LangProvider>
-        <App />
-      </LangProvider>
-    </GoogleOAuthProvider>
-  </StrictMode>,
+  <GoogleOAuthProvider clientId="678553599072-5ncop5mo1558m4sdvrim2iidscj1224t.apps.googleusercontent.com">
+    <LangProvider>
+      <App />
+    </LangProvider>
+  </GoogleOAuthProvider>,
 );
